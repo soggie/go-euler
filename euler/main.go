4a8faf8c94ab5a667224ea8249c7c16a748585e7
Ruben LZ Tan <soggie@gmail.com>
2016-02-24 15:05:06 +0800

    Done with p002

--- euler/main.go
+++ euler/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	p001 "euler/p001"
-	"fmt"
+	// p001 "euler/p001"
+	// p002 "euler/p002"
+	// "fmt"
 )
 
 func main() {
-	fmt.Printf("p001=%v\n", p001.Simple(1000, false))
+	// p002.Simple()
+	// fmt.Printf("p001=%v\n", p001.Simple(1000, false))
 }
